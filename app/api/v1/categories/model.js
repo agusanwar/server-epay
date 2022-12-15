05aa425493078ee16a2cdd49426ccde65377fafa
agusanwar <agusanwar@gmail.com>
2022-12-15 17:28:55 +0700

    add age

--- app/api/v1/categories/model.js
+++ app/api/v1/categories/model.js
@@ -10,9 +10,6 @@ let categorySchema = mongoose.Schema(
         },
         age:{
             type: Number,
-            minLength: [3, 'Minimal Length Category 3 Karakter'],
-            maxLength: [20, 'Maximal Length Category 20 Karakter'],
-            required: [true, 'Nama Category harus diisi'],
         }
     },
     { timestamps: true }
